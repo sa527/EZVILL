49945be3e205d646d6aea8e7d4d2d597e7ae08e6
a1746393 <linbiao.li@student.adelaide.edu.au>
2022-09-07 16:32:46 +0930

    This is signup had changed

--- app/src/main/java/com/example/ezvill/signup.java
+++ app/src/main/java/com/example/ezvill/signup.java
@@ -1,5 +1,6 @@
 package com.example.ezvill;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,10 +11,17 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class signup extends AppCompatActivity {
     private EditText msignupemail,msignuppassword;
     private RelativeLayout msignup;
     private TextView mgotologin;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +31,7 @@ public class signup extends AppCompatActivity {
         msignuppassword=findViewById(R.id.signuppassword);
         msignup=findViewById(R.id.signup);
         mgotologin=findViewById(R.id.gotologin);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         mgotologin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,9 +54,44 @@ public class signup extends AppCompatActivity {
             }
             else{
                 //////register
+                firebaseAuth.createUserWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Registeration Successgul", Toast.LENGTH_SHORT).show();
+                            sendEmailVerification();
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Failed to Register", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
             }
 
             }
         });
+    }
+    //send mail verification
+    private void sendEmailVerification()
+    {
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+        if (firebaseUser!=null)
+        {
+            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(getApplicationContext(), "Verification Email is Send, Verify and Loogin", Toast.LENGTH_SHORT).show();
+                    firebaseAuth.signOut();
+                    finish();
+                    startActivity(new Intent(signup.this,MainActivity.class));
+                }
+            });
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Failed Send registration Email", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
